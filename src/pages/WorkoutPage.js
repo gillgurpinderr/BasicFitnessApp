60e682146ca594a1368c9d68aee915a72b80639f
G <g.singh@Gs-MacBook-Air.local>
2023-04-14 22:11:38 -0400

    latest-code

--- src/pages/WorkoutPage.js
+++ src/pages/WorkoutPage.js
@@ -1,34 +1,90 @@
-import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
 
-const WorkoutPage = ({ route }) => {
-    const { calorieIntake, bmi } = route.params;
+const WorkoutPage = () => {
+  const [duration, setDuration] = useState('');
+  const [weight, setWeight] = useState('');
+  const [workouts, setWorkouts] = useState([]);
 
-    return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Workout Plan</Text>
-            <Text style={styles.text}>Your BMI: {bmi.toFixed(2)}</Text>
-            <Text style={styles.text}>Daily Calorie Intake: {calorieIntake.toFixed(0)} calories</Text>
-            {/* Display workout plans based on user's profile */}
+  const fetchWorkouts = async () => {
+    try {
+      const response = await fetch(`https://trackapi.nutritionix.com/v2/natural/exercise?query=${weight}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'x-app-id': '583e5473',
+          'x-app-key': 'd9b315fa0a6c358e10727e19b9c4de89',
+          'x-remote-user-id': '0'
+        },
+        body: JSON.stringify({
+          query: `I lifted ${weight} lbs for ${duration} minutes`
+        })
+      });
+      const json = await response.json();
+      setWorkouts(json.exercises);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Log your workout:</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Duration (minutes)"
+        value={duration}
+        onChangeText={setDuration}
+        keyboardType="numeric"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Weight (lbs)"
+        value={weight}
+        onChangeText={setWeight}
+        keyboardType="numeric"
+      />
+      <Button title="Add Workout" onPress={fetchWorkouts} />
+      {workouts.map((workout, index) => (
+        <View key={index} style={styles.workoutContainer}>
+          <Text style={styles.workoutText}>
+            {workout.name}: {workout.nf_calories} calories burned
+          </Text>
         </View>
-    );
+      ))}
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        paddingHorizontal: 20,
-    },
-    title: {
-        fontSize: 24,
-        textAlign: 'center',
-        marginBottom: 20,
-    },
-    text: {
-        fontSize: 18,
-        marginBottom: 10,
-    },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 20
+  },
+  input: {
+    width: 200,
+    height: 40,
+    padding: 10,
+    marginBottom: 20,
+    backgroundColor: '#eee',
+    borderRadius: 10
+  },
+  workoutContainer: {
+    backgroundColor: '#ddd',
+    padding: 10,
+    borderRadius: 10,
+    marginVertical: 5,
+    width: 300
+  },
+  workoutText: {
+    fontSize: 16
+  }
 });
 
 export default WorkoutPage;
